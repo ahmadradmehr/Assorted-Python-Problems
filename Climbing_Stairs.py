23d1d1a9aa9675508a541f8407555f910ab03b2f
Ahmad Radmehr <radmehr@vt.edu>
2021-05-25 11:47:33 -0400

    Create "climbing Stairs" Solution

--- Climbing_Stairs.py
+++ Climbing_Stairs.py
@@ -69,11 +69,11 @@ Constraints:
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
-            return 1;
-        first = 1;
-        second = 2;
+            return 1
+        first = 1
+        second = 2
         for i in range(2, n):
-            third = first + second;
-            first = second;
-            second = third;
-        return second;
+            third = first + second
+            first = second
+            second = third
+        return second
